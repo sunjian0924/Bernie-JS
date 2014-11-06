b9bec55dd54d708aa6823ef44e65465caaeeea5e
sunjian0924 <sunj3@miamioh.edu>
2014-11-06 13:45:14 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -58,6 +58,7 @@ app.use('/', function(req, res) {
       cas.authenticate(req, res, function(err, status, username, extended) {
         if (err) {
           // Handle the error
+          console.log(err);
           res.send({error: err});
         } else {
           // Log the user in 
