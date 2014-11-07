fd3e48dd1e5c609b335ec3eb60e094df4b5040e0
Jian Sun <jiansun@Jians-MacBook-Pro-2.local>
2014-11-07 16:12:22 -0500

    separate clients

--- server/server.js
+++ server/server.js
@@ -59,7 +59,10 @@ app.use(morgan(':remote-addr :method :url :status'));
 //app.use(express.static(path.join(application_root, '../client/auth')));
 
 //cas happends here
-app.use('/', cas.bouncer, express.static(path.join(application_root, '../client/clientApp')));
+//app.use('/', cas.bouncer, express.static(path.join(application_root, '../client/clientApp')));
+app.use('/', cas.bouncer, function(req, res) {
+	res.sendFile(path.join(application_root, '../client/clientApp'));
+});
 app.get('/logout', cas.logout);
 	
 
